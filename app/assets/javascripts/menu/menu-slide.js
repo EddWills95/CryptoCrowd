a44889b437a85a37f10f5d343108ff0afa855942
Edd Williams <edd.williams@me.com>
2017-08-14 22:35:24 +0100

    Menu now moving in and out of frame nicely

--- app/assets/javascripts/menu/menu-slide.js
+++ app/assets/javascripts/menu/menu-slide.js
@@ -1,7 +1,15 @@
 $(function() {
   var state = "closed";
+  var menu = $('#hidden-nav');
   $("#menu-bars").on("click", function() {
     menuAnimate(this);
+    if(state == "closed") {
+      document.getElementById('hidden-nav').style.width = "35%";
+      state = "open";  
+    } else {
+      document.getElementById('hidden-nav').style.width = "0";  
+      state = "closed";
+    }
   });
 })
 
