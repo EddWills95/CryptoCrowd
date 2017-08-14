42e7b52b28f9c7b784530b54b4f78fd374581b7d
Edd Williams <edd.williams@me.com>
2017-08-14 23:00:40 +0100

    Working on animation and styling for hidden nav bar

--- app/assets/javascripts/menu/menu-slide.js
+++ app/assets/javascripts/menu/menu-slide.js
@@ -1,14 +1,28 @@
 $(function() {
   var state = "closed";
   var menu = $('#hidden-nav');
+  var bodyObject = $('#main-body')[0];
+  var menuLines = []
+  menuLines.push(document.getElementsByClassName('bar1')[0]);
+  menuLines.push(document.getElementsByClassName('bar2')[0]);
+  menuLines.push(document.getElementsByClassName('bar3')[0]);
+
   $("#menu-bars").on("click", function() {
     menuAnimate(this);
     if(state == "closed") {
+      state = "open"; 
       document.getElementById('hidden-nav').style.width = "35%";
-      state = "open";  
+      bodyObject.className += "greyed-out";
+      for(var i = 0; i < menuLines.length; i++) {
+        menuLines[i].style.backgroundColor = "white";
+      } 
     } else {
-      document.getElementById('hidden-nav').style.width = "0";  
       state = "closed";
+      document.getElementById('hidden-nav').style.width = "0"; 
+      bodyObject.className -= "greyed-out"; 
+      for(var i = 0; i < menuLines.length; i++) {
+        menuLines[i].style.backgroundColor = "black";
+      } 
     }
   });
 })
