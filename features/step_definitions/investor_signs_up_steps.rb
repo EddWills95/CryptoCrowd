c821d248f8e868cc2f5f0f0e072b50c2f554dc4d
Edd Williams <edd.williams@me.com>
2017-08-14 14:49:24 +0100

    Investor Sign in Feature with cucumber .. WIP

---
+++ features/step_definitions/investor_signs_up_steps.rb
@@ -0,0 +1,35 @@
+Given(/^They are on the investor sign up page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^they enter their email$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^they enter their password$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^they conform their password$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^they register$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^they have a new account$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^they get sent to the homepage$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^they see a message welcoming them$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^they recieve an email asking them to confirm their email$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
