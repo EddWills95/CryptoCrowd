cb004e38166aac962505760612fa8f9450ac3ad8
Edd Williams <edd.williams@me.com>
2017-08-15 16:00:59 +0100

    Foriegn key issues resolved

--- db/schema.rb
+++ db/schema.rb
@@ -33,11 +33,6 @@ ActiveRecord::Schema.define(version: 20170815130010) do
     t.index ["trader_id"], name: "index_propositions_on_trader_id"
   end
 
-  create_table "traders", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -60,5 +55,4 @@ ActiveRecord::Schema.define(version: 20170815130010) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "propositions", "traders"
 end
