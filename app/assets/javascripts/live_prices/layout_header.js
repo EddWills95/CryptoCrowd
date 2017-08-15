910fa80fb621fe8ea624f62708b5b7ebc4494ac9
Edd Williams <edd.williams@me.com>
2017-08-15 22:01:06 +0100

    Crypto Tickers & Further styling to hidden nav.

--- app/assets/javascripts/live_prices/layout_header.js
+++ app/assets/javascripts/live_prices/layout_header.js
@@ -1,12 +1,23 @@
 $(function() { 
   var socket = io('https://streamer.cryptocompare.com/');
 
-  var subscription = ['2~Coinbase~BTC~USD'];
+  var subscription = ['2~Coinbase~BTC~USD', '2~Coinbase~ETH~USD', '2~Coinbase~LTC~USD'];
 
   socket.emit('SubAdd', {subs:subscription} );
 
   socket.on("m", function(message){
-    var coinbase_btc = (CCC.CURRENT.unpack(message).PRICE);
-    $('#btc-price')[0].innerHTML = coinbase_btc;     
+    var unpacked = CCC.CURRENT.unpack(message);
+    if(unpacked.FROMSYMBOL == "BTC"){
+      var coinbase_btc = (CCC.CURRENT.unpack(message).PRICE);
+      $('#btc-price')[0].innerHTML = coinbase_btc;
+    }
+    if(unpacked.FROMSYMBOL == "ETH"){
+      var coinbase_btc = (CCC.CURRENT.unpack(message).PRICE);
+      $('#eth-price')[0].innerHTML = coinbase_btc;
+    }
+    if(unpacked.FROMSYMBOL == "LTC"){
+      var coinbase_btc = (CCC.CURRENT.unpack(message).PRICE);
+      $('#ltc-price')[0].innerHTML = coinbase_btc;
+    }
   });
 });
