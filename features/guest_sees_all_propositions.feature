d8909647e9dfcd9f4ed9a2da041f9fe0a245c430
Edd Williams <edd.williams@me.com>
2017-08-16 13:39:56 +0100

    User sees propositions on homepage

--- features/guest_sees_all_propositions.feature
+++ features/guest_sees_all_propositions.feature
@@ -4,4 +4,6 @@ Feature: A guest visits the site
   Given there are some propositions
     And they are on the homepage
   Then they see the propositions
-    And they see the currencies involved
+    And they see the names currencies involved
+    And they see the trader who proposed it
+    And they see the number of votes it has
--- features/step_definitions/guest_sees_all_propositions_steps.rb
+++ features/step_definitions/guest_sees_all_propositions_steps.rb
@@ -1,16 +1,24 @@
 Given(/^there are some propositions$/) do
-  @trader = Trader.new(email: "example@somewhere.org", password: "password", password_confirmation: "password")
-  @trader.skip_confirmation!
-  @trader.save
-  @proposition1 = Proposition.create!(title: "Buy Bitcoins!", description: "Bitcoin is up up up", currency1_id: 1, currency2_id: 2, trader_id: @trader.id)
-  @proposition2 = Proposition.create!(title: "Sell!", description: "$4300 is top", currency1_id: 1, currency2_id: 2, trader_id: @trader.id)
+  @currency1 = Currency.create!(name: "BTC")
+  @currency2 = Currency.create!(name: "USD")
+  @trader = Trader.create!(email: "someone@somewhere.com", name: "Max Keiser", password: "password", password_confirmation: "password")
+  @proposition = Proposition.create!(title: "Buy!!", description: "please", currency_to_id: @currency1.id, currency_from_id: @currency2.id, trader_id: @trader.id, votes: 325)
+  @proposition2 = Proposition.create!(title: "Sell!", description: "$4300 is top", currency_to_id: @currency1.id, currency_from_id: @currency2.id, trader_id: @trader.id, votes: 132)
 end
 
 Then(/^they see the propositions$/) do
-  expect(page).to have_content("Buy Bitcoins!")
+  expect(page).to have_content("Buy!!")
 end
 
-Then(/^they see the currencies involved$/) do
+Then(/^they see the names currencies involved$/) do
   expect(page).to have_content("BTC")
   expect(page).to have_content("USD") 
+end
+
+Then(/^they see the trader who proposed it$/) do
+  expect(page).to have_content("Max Keiser")
+end
+
+Then(/^they see the number of votes it has$/) do
+  expect(page).to have_content("325")
 end
