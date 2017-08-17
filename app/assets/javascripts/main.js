4ce1100b30e610024f2d0c0a90057f231493d96f
Edd Williams <edd.williams@me.com>
2017-08-17 11:22:49 +0100

    Current User_Id javascript broken. now fixed

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,17 +2,21 @@
 // All this logic will automatically be available in application.js.
 
 $(function() {
-  var currentId = document.getElementById('current_id').value;
-
-  var voteElements = document.getElementsByClassName('vote-button');
-  for(var i = 0; i < voteElements.length; i++) {
-    voteElements[i].addEventListener("click", function(){
-      console.log("clicked");
-      var action = this.id.split("-")[0];
-      var id = this.id.split("-")[1];
-      $.ajax({
-        url: "http://localhost:3000/propositions/" + id + "/" + action + "?user_id=" + currentId
+  var currentId;
+  if(document.getElementById('current_Id') == undefined) {
+    null
+  } else {
+    current_Id = document.getElementById('current_Id').value
+    var voteElements = document.getElementsByClassName('vote-button');
+    for(var i = 0; i < voteElements.length; i++) {
+      voteElements[i].addEventListener("click", function(){
+        console.log("clicked");
+        var action = this.id.split("-")[0];
+        var id = this.id.split("-")[1];
+        $.ajax({
+          url: "http://localhost:3000/propositions/" + id + "/" + action + "?user_id=" + currentId
+        })
       })
-    })
+    }
   }
 })
