4787d5c0d02e3001e92b28983fe003793fac2988
Edd Williams <edd.williams@me.com>
2017-08-20 14:06:37 +0100

    Added similar test for trader

--- features/step_definitions/registered_user_has_wallet_steps.rb
+++ features/step_definitions/registered_user_has_wallet_steps.rb
@@ -3,7 +3,6 @@ When(/^they click on Wallet$/) do
 end
 
 Then(/^they see a list of all their coins$/) do
-  save_and_open_page
   expect(page).to have_content "BTC:"
   expect(page).to have_content "ETH:"
   expect(page).to have_content "LTC:"
