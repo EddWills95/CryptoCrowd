9d981faf6ef3787a7b76c850614a60cd374d24d0
Edd Williams <edd.williams@me.com>
2017-08-22 17:38:53 +0100

    Optimization changes with render-later

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,15 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :current_user
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :bio, :favourite_coin])
+  end
+
+  def current_user
+    @trader = current_trader if current_trader
+    @investor = current_investor if current_investor
   end
 end
