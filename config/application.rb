d9293fff0b94b7fc389989c6dbc81c36118d48f1
Edd Williams <edd.williams@me.com>
2017-08-23 18:48:09 +0100

    Moved action mailer into application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module CryptoCrowd
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.action_mailer.default_url_options = { :host => 'cryptocrowd.herokuapp.com' }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
